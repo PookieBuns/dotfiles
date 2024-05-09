d35f767d51a4b5d44cf3008129f41a47619aa12a
allenyuchen <allenyuchen@hotmail.com>
2024-05-09 13:48:55 +0800

    ruff is already LSP so you don't need it as linter

--- dot_config/nvim/lua/plugins/linting.lua
+++ dot_config/nvim/lua/plugins/linting.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		local lint = require("lint")
 		lint.linters_by_ft = {
-			python = { "ruff" },
+			python = { "mypy" },
 		}
 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 			callback = function()
