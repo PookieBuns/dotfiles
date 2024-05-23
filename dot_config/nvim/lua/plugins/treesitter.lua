13c0105e434976d25ca7130b471e81ddaf11cc87
allenyuchen <allenyuchen@hotmail.com>
2024-05-23 17:09:46 +0800

    feat: add treesitter folding

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -45,6 +45,9 @@ return {
 		},
 		config = function(_, opts)
 			require("nvim-treesitter.configs").setup(opts)
+			vim.opt.foldmethod = "expr"
+			vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+			vim.opt.foldenable = false
 		end,
 	},
 	-- Context
